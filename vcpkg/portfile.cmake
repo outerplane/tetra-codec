7c6679b1dec1c304421efddb3698587210ad8744
Geurt Vos <geurt.vos@outerplane.nl>
2025-12-17 14:33:49 +0100

    updated vcpkg files

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -1,39 +1,14 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
-include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/tetra-codec-1.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "file:///${CURRENT_PORT_DIR}/tetra-codec-VERSION.zip"
-    FILENAME "tetra-codec-VERSION.zip"
-    SHA512 CHECKSUM
-)
-vcpkg_extract_source_archive(${ARCHIVE})
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO outerplane/tetra-codec
+	REF 2604d923fccefe109e3138b8f3d873876a870105
+	SHA512 a8812bd0a470be61d6d1c18aef38be89291181d9de3e4b1457c6026bc0652bb5c70c88211305eb89d9ba9a6ada4fd223bdf632b91fe5fed001bfbb3af5643439
+	HEAD_REF master
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_install()
+vcpkg_fixup_pkgconfig()
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(RENAME ${CURRENT_PACKAGES_DIR}/lib/tetra-codec.dll ${CURRENT_PACKAGES_DIR}/bin/tetra-codec.dll)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/tetra-codec.dll ${CURRENT_PACKAGES_DIR}/debug/bin/tetra-codec.dll)
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-file(INSTALL ${SOURCE_PATH}/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/tetra-codec RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/README.md")
